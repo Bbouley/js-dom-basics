7bb5d40da8c084b50982a4a4e98a61bd00110f8b
bbouley <b.bouley@hotmail.co.uk>
2015-07-21 13:57:42 -0600

    added functions from exercise to test editing html with DOM

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('Sanity Check!!')
+console.log('Sanity Check!!');
+
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements);
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement);
+
+var allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i].innerText = i);
+}
+
+var li = document.createElement('li');
+console.log(li);
+
+var newElement = li.innerText = 'another list item';
+console.log(newElement);
+console.log(li);
+
+getSingleElement.appendChild(li);
+console.log(allListItems);
+
+var anotherListElement = document.createElement('li');
+anotherListElement.innerText = 'prepend';
+console.log(anotherListElement);
+
+getSingleElement.insertBefore(anotherListElement, getSingleElement.firstChild);
+console.log(allListItems);
